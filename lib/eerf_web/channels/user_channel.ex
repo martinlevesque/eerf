c652b7d5e5b0470f37493f39e03cb026932a50ca
Martin Levesque <levesque.martin@gmail.com>
2018-12-03 21:42:46 -0500

    test recv board

--- lib/eerf_web/channels/user_channel.ex
+++ lib/eerf_web/channels/user_channel.ex
@@ -12,12 +12,16 @@ defmodule EerfWeb.UserChannel do
   end
 
   def handle_in("get-board", %{"board_name" => board_name}, socket) do
-    ttest = Eerf.Rooms.get_room_or_create(board_name)
+    room = Eerf.Rooms.get_room_or_create(board_name)
     IO.puts "get board.. test "
-    IO.inspect ttest
+    IO.inspect room
 
-    board_data = %{ "im": "data" }
-    broadcast!(socket, "broadcast", %{board_data: board_data})
+    board_data = [
+      %{color: "#01ff70", id: "ljp944le07", size: "10", tool: "Pencil", type: "line"},
+      %{parent: "ljp944le07", tool: "Pencil", type: "child", x: 253, y: 95}
+    ]
+
+    broadcast!(socket, "recv-initial-board", %{board_data: board_data})
     {:reply, :ok, socket}
   end
 end
