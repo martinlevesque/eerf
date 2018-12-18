461c6313883a19b04c576ad4af413a65d503d902
Martin Levesque <levesque.martin@gmail.com>
2018-12-17 19:54:26 -0500

    new deploy ci

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -2,4 +2,8 @@
 
 echo "{\"token\":\"${OPENODE_TOKEN}\",\"site_name\":\"eerf\",\"instance_type\":\"server\"}" > .openode
 
-openode status #deploy -t ${OPENODE_TOKEN} -s eerf
+openode locations
+
+openode status
+
+openode sync-n-reload
